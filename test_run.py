829b57fdf6550be1d174471ceea88cc1a5c73f77
Aaron Wang <aaron51593@gmail.com>
2020-08-07 15:27:30 -0400

    Adaboost_1

--- test_run.py
+++ test_run.py
@@ -4,14 +4,19 @@ from sklearn.model_selection import train_test_split, KFold
 from sklearn import preprocessing
 from adaboost import Adaboost
 
+# define the accuracy calculation function
 def accuracy(true_label, prediction):
     accuracy = np.sum(true_label == prediction) / len(true_label)
     return accuracy 
-
+    
+## file location is subject to change
 data_set    =  pd.read_csv(r'/home/aaron/Desktop/car.data',header=0)
 #print (data_set.head())
-# header info
-#buying,maint,door,persons,lug_boot,safety,target_label
+
+# header info (reference)
+# buying,maint,door,persons,lug_boot,safety,target_label
+
+# convert all the non-int value to numeric value using sklearn preprocessing labelencoder
 le = preprocessing.LabelEncoder()
 buying = le.fit_transform(list(data_set["buying"]))
 maint = le.fit_transform(list(data_set["maint"]))
@@ -24,7 +29,7 @@ target_label = le.fit_transform(list(data_set["target_label"]))
 label      =   np.array(target_label)
 attributes =   np.array(list(zip(buying,maint,door,persons,lug_boot,safety)))
 
-
+# cross validation using k fold from sklearn
 kf = KFold(n_splits=5,shuffle=True)
 counter =0 
 for train_index, test_index in kf.split(attributes): 
@@ -32,7 +37,7 @@ for train_index, test_index in kf.split(attributes):
     x_train, x_test = attributes[train_index], attributes[test_index]
     y_train, y_test = label[train_index], label[test_index]
     #print("train set size: %d,test set size: %d" %(len(x_train),len(x_test)))
-    
+ # change the number of stump of the adaboost, and onserve
     counter=counter+1
     for i in range (10):
         Ada_clf = Adaboost(n_clf=i+1)
