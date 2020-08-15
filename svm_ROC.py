8b11ea191da9c8020565d54a64328200abd390ce
coconut009 <aaron51593.com>
2020-08-15 10:54:40 -0400

    SVM_ROC

--- svm_ROC.py
+++ svm_ROC.py
@@ -1,45 +1,35 @@
 import numpy as np
-from time import time
 import pandas as pd
 
 from sklearn import preprocessing
 from sklearn.model_selection import  KFold
-from sklearn.tree import DecisionTreeClassifier
-from sklearn. ensemble import AdaBoostClassifier
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
+
 
 import matplotlib.pyplot as plt
 from sklearn.metrics import auc
 from sklearn.metrics import plot_roc_curve
 
-###############################################################################
-# define the accuracy calculation function
-def accuracy(true_label, prediction):
-    accuracy = np.sum(true_label == prediction) / len(true_label)
-    return accuracy
+
 
 ###############################################################################
 ## file location is subject to change
-data_set_1    =  pd.read_csv(r'car_2class.data',header=0)
-data_set_2    = np.loadtxt('/home/aaron/Desktop/page-blocks.txt')
+data_set    =  pd.read_csv(r'car_2class.data',header=0)
+
 
 le = preprocessing.LabelEncoder()
-buying = le.fit_transform(list(data_set_1["buying"]))
-maint = le.fit_transform(list(data_set_1["maint"]))
-door = le.fit_transform(list(data_set_1["door"]))
-persons = le.fit_transform(list(data_set_1["persons"]))
-lug_boot = le.fit_transform(list(data_set_1["lug_boot"]))
-safety = le.fit_transform(list(data_set_1["safety"]))
-target_label = list(data_set_1["target_label"])
+buying = le.fit_transform(list(data_set["buying"]))
+maint = le.fit_transform(list(data_set["maint"]))
+door = le.fit_transform(list(data_set["door"]))
+persons = le.fit_transform(list(data_set["persons"]))
+lug_boot = le.fit_transform(list(data_set["lug_boot"]))
+safety = le.fit_transform(list(data_set["safety"]))
+target_label = list(data_set["target_label"])
 # zip all the transformed data into numpy array
-label_1     =   np.array(target_label)
-attributes_1=   np.array(list(zip(buying,maint,door,persons,lug_boot,safety)))
+label     =   np.array(target_label)
+attributes=   np.array(list(zip(buying,maint,door,persons,lug_boot,safety)))
 
-# second data set
-label_2     =   data_set_2[:,-1]
-attributes_2=   data_set_2[:,:-1]
 
 tprs = []
 aucs = []
@@ -48,19 +38,17 @@ fig, ax = plt.subplots()
 
 for i in range (2,7):
     kf = KFold(n_splits=i,shuffle=True)
-    print("Data Set 1 with the number of spilt is", i)
-    for train_index, test_index in kf.split(attributes_1):
-        x_1_train, x_1_test = attributes_1[train_index], attributes_1[test_index]
-        y_1_train, y_1_test = label_1[train_index], label_1[test_index]
-        n_components = attributes_1.shape[1]
-        pca = PCA(n_components=n_components, svd_solver='randomized',whiten=True).fit(x_1_train)
-        x_train_pca = pca.transform(x_1_train)
-        X_test_pca = pca.transform(x_1_test)
+    for train_index, test_index in kf.split(attributes):
+        x_train, x_test = attributes[train_index], attributes[test_index]
+        y_train, y_test = label[train_index], label[test_index] 
+        n_components = attributes.shape[1]
+        pca = PCA( n_components, svd_solver='randomized',whiten=True).fit(x_train)
+        x_train_pca = pca.transform(x_train)
+        x_test_pca = pca.transform(x_test)
         svc_clf = SVC(C=1000, class_weight='balanced', gamma=0.05)
         svc_clf = svc_clf.fit(x_train_pca, y_train)
-        svc_pred = svc_clf.predict(X_test_pca)
-        acc = accuracy(y_test,svc_pred)
-        viz = plot_roc_curve(svc_clf,x_1_test,y_1_test,name='ROC fold {}'.format(i),alpha=0.3, lw=1, ax=ax)
+        svc_pred = svc_clf.predict(x_test_pca)        
+        viz = plot_roc_curve(svc_clf,x_test_pca,y_test,name='ROC @ fold {} times'.format(i),alpha=0.3, lw=1, ax=ax)
         interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
         interp_tpr[0] = 0.0
         tprs.append(interp_tpr)
@@ -72,8 +60,10 @@ mean_tpr = np.mean(tprs, axis=0)
 mean_tpr[-1:] = 1.0
 mean_auc = auc(mean_fpr, mean_tpr)
 std_auc = np.std(aucs)
+
+
 ax.plot(mean_fpr, mean_tpr, color='b',
-        label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+        label=r'Mean ROC (AUC = %0.4f $\pm$ %0.4f)' % (mean_auc, std_auc),
         lw=2, alpha=.8)
 
 std_tpr = np.std(tprs, axis=0)
@@ -83,7 +73,7 @@ ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
                 label=r'$\pm$ 1 std. dev.')
 
 ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
-       title="Receiver operating characteristic example")
+       title="SVM Classifier ROC curve of Data Set 1")
 ax.legend(loc="lower right")
 plt.show() 
          
